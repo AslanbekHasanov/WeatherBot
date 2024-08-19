b5360afdd076d7c41778d331ecb8c207762f2a1d
AslanbekHasanov <learneraslanbek1220@gmail.com>
2024-08-19 16:34:43 +0500

    BROKERS: Initialize TelegramBot Broker

---
+++ WeatherBot.API/Brokers/TelegramBot/TelegramBotBroker.cs
@@ -0,0 +1,17 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace WeatherBot.API.Brokers.TelegramBot
+{
+ 
+    public partial class TelegramBotBroker : ITelegramBotBroker
+    {
+        private readonly string token = @"7252396202:AAEd79QdzHwr2t6vQ8tlehb1Uxu4EwVcBhQ";
+        private readonly ITelegramBotClient client;
+
+        public TelegramBotBroker()
+        {
+            this.client = new TelegramBotClient(token);
+        }
+    }
+}
