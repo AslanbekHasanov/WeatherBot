812c56e8d833813132d66637dc324535f219f131
AslanbekHasanov <learneraslanbek1220@gmail.com>
2024-08-19 16:18:07 +0500

    BROKERS: Initialize WeatherBotBroker

---
+++ WeatherBot.API/Brokers/Weathers/IWeatherBotBroker.cs
@@ -0,0 +1,9 @@
+﻿using WeatherBot.API.Models.Weathers;
+
+namespace WeatherBot.API.Brokers.Weathers
+{
+    internal interface IWeatherBotBroker
+    {
+        Task<Root> GetWeatherInfo(double latitude, double longtidue);
+    }
+}
